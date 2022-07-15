0732e427dcd0e2371293aa1a5a1d3fc195cac06a
jingshuang1 <jingshuang1@staff.weibo.com>
2022-07-15 16:39:41 +0800

    webpack problem

--- three/boxGeometry.js
+++ three/boxGeometry.js
@@ -2,6 +2,7 @@
 // 如何在canvas上设置鼠标事件
 let myCanvas = document.getElementById('myCanvas')
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xAAAAAA);
 
 const camra = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,100)
 camra.position.z = 4
@@ -14,10 +15,22 @@ const martial = new THREE.MeshPhongMaterial({ color : 	0x00ff00 })
 const cube = new THREE.Mesh(box,martial)
 
 const light = new THREE.DirectionalLight(0xffffff,1)
-light.position.set(-1, 2, 4);
+light.position.set(10, 10, 10);
 
+const light1 = new THREE.DirectionalLight(0xffffff,1)
+light.position.set(-10, -10, -10);
+
+// 控制器的操作
+const controls = new THREE.OrbitControls(camra,renderer.domElement)
+// controls.enableZoom = false  这个控制器咋没啥用呢
+// controls.autoRotate = true
+
+
+scene.add(light1)
 scene.add(light)
 scene.add(cube)
+
+// 控制器的操作
 
 function animate(){
   cube.rotation.x += 0.01
@@ -25,6 +38,7 @@ function animate(){
   renderer.render(scene, camra)
   renderer.setSize(myCanvas.clientWidth, myCanvas.clientHeight)  //边缘块状化
   requestAnimationFrame(animate)
+  // controls.update() // 但是没有任何效果
 }
 
 animate()
