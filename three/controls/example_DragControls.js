188ea36a2b4bc16fd85d21578b5b250d1a35db79
jingshuang1 <jingshuang1@staff.weibo.com>
2022-08-22 10:36:24 +0800

    add

--- three/controls/example_DragControls.js
+++ three/controls/example_DragControls.js
@@ -13,7 +13,62 @@ renderer.setSize(canvas.clientWidth,canvas.clientHeight)
 
 const objects = []
 let controls = null
-function init(){
+let enableSelection = false
+const mouse = new THREE.Vector2(); // 这两个不知道是什么意识呢
+const raycaster = new THREE.Raycaster(); //在三维空间中用来计算出鼠标移动过了什么物体
+const group = new THREE.Group() // 创建一个组
+scene.add(group)
+onInit()
+eventBind()
+render()
+
+
+
+function eventBind(){
+  if(controls) controls.addEventListener('drag',render) // 拖拽完成之后进行canvas重新渲染
+  window.addEventListener('resize',onResize)
+  window.addEventListener('keydown', onKeydown)
+  window.addEventListener('keyup',onKeyUp)
+  window.addEventListener('click', onClick)
+}
+
+// 点击事件
+function onClick(event){
+  event.preventDefault();
+  if(enableSelection){
+    const draggableObjects = controls.getObjects() // 获取控制器中的所有的元素
+    draggableObjects.length = 0;
+    // 这里是啥意识呢？
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+		mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    // 这个是啥意识呢？
+    raycaster.setFromCamera(mouse, camera)
+    const intersections = raycaster.intersectObjects( objects, true );
+    if(intersections.length > 0){
+      const object = intersections[ 0 ].object;
+      if(group.children.includes( object )){
+        object.material.emissive.set( 0xaaaaaa );
+				group.attach( object );
+      }
+      controls.transformGroup = true;
+      draggableObjects.push( group );
+    }
+    if ( group.children.length === 0 ) {
+      controls.transformGroup = false; // 设置是true的时候是将拖动整个组而不是单个元素
+      draggableObjects.push( ...objects );
+    }
+  }
+  render()
+}
+
+function onResize(){
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
+}
+// 初始化创建元素
+function onInit(){
   const light = new THREE.AmbientLight( 0x505050 )
   const light1 = new THREE.SpotLight( 0xffffff, 1.5 );
   light1.position.set( 0, 500, 2000 );
@@ -24,6 +79,7 @@ function init(){
   light1.shadow.mapSize.width = 1024;
   light1.shadow.mapSize.height = 1024;
   const geometry = new THREE.BoxGeometry( 40, 40, 40 );
+  // 初始化出来数据
   for(let i=0; i< 200; i++){
     const object = new THREE.Mesh(geometry,new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff}))
 
@@ -49,8 +105,15 @@ function init(){
   scene.add(light1)
 }
 
-init()
+// 渲染屏幕
 function render(){
   renderer.render(scene, camera)
 }
-render()
+
+function onKeydown(event){ // shift
+  enableSelection = ( event.keyCode === 16 ) ? true : false;
+}
+function onKeyUp() {
+  enableSelection = false;
+}
+
