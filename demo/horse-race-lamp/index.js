b2bc235b1c6bbe1c7d3aae65c975b5e05174d6b7
jingshuang1 <jingshuang1@staff.weibo.com>
2022-09-06 10:40:37 +0800

    九宫格旋转抽奖

--- demo/horse-race-lamp/index.js
+++ demo/horse-race-lamp/index.js
@@ -1,59 +1,56 @@
 let index = 0
 let prize = 0 // 获奖的索引值
-let speed = 500
+let speed = 120
 let timeOut = null
 let resultIndex = -1
-let times = 50 // 至少旋转多少圈
+let times = 0 // 旋转了多少圈
+let baseTimes = random(80,120)// 至少转动
+let isClick = false
+
 let domItems = []
 let length = document.getElementsByTagName('li').length - 1
-
-let lotteryBtn = document.getElementById('turntable-btn-li')
-
-lotteryBtn.addEventListener('click', onLotteryClick)
-
-function onLotteryClick(){
-  onShowAnimate()
-  window.setTimeout(()=>{
-    resultIndex = 3
-  },2000)
-}
-
-function onInit(){
-  resultIndex = -1
-}
-
-
 for(let i=1; i<=length ;i++){
   let item = document.getElementsByClassName(`lamp-li-${i}-img`)[0]
   domItems.push(item)
 }
 
 
+let lotteryBtn = document.getElementById('turntable-btn-li')
+lotteryBtn.addEventListener('click', onLotteryClick)
+
+function onLotteryClick(){
+  if(isClick) return
+  isClick = true
+  onShowAnimate()
+  window.setTimeout(()=>{
+    resultIndex = random(0, 7) // 随机生成
+  },2000)
+}
+  
 function onShowAnimate(){
-  times --
+  times ++
   if(index > length - 1 ) index = 0
   domItems.forEach(item =>{
     item.classList.remove("on");
   })
   domItems[index].classList.add("on")
-  console.log(times,index)
-  if(resultIndex > -1 && index === resultIndex && times < 0){
+  if(resultIndex > -1 && index === resultIndex && times > baseTimes+8 ){
     window.clearTimeout(timeOut)
     resultIndex = -1
+    isClick = false
   }else{
     index += 1
-    if(resultIndex > - 1){
-      if(speed > 500){
-        speed = 500
-      }else{
-        speed += 50
-      }
+    if( times < baseTimes ){
+      speed -= 5
     }else{
-      if(speed < 200){
-        speed = 100
-      }else if(speed < 1000) speed -= 50
+      speed += 25
     }
+    if(speed < 20) speed = 20
     timeOut = window.setTimeout(onShowAnimate,speed)
   }
 }
-// 现在还剩下旋转速度的操作和随机生成一个结果值的情况
+// 随机生成整数
+function random(n, m){
+	var c = m-n+1;  
+  return Math.floor(Math.random() * c + n);
+}
