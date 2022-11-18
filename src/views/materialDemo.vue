b8b06c6dd770123c88530b79d546519df62c0b3a
jingshuang1 <jingshuang1@staff.weibo.com>
2022-11-18 16:29:07 +0800

    re

--- src/views/materialDemo.vue
+++ src/views/materialDemo.vue
@@ -11,11 +11,12 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5
 const renderer = new THREE.WebGLRenderer();
-let  orbitControls = null
-const onAnimate = () =>{
+scene.background = new THREE.Color(0xAAAAAA);
+let orbitControls = null
+const onAnimate = () => {
   requestAnimationFrame(onAnimate)
   renderer.render(scene, camera)
-  if(orbitControls){
+  if (orbitControls) {
     orbitControls.update()
   }
 }
@@ -25,22 +26,28 @@ onMounted(() => {
   let baseDom = document.getElementById('base')
   baseDom.appendChild(renderer.domElement);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  orbitControls =  new OrbitControls(camera, renderer.domElement)
-  const axesHelper = new THREE.AxesHelper( 5 );
-  scene.add( axesHelper )
+  orbitControls = new OrbitControls(camera, renderer.domElement)
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper)
   // 给元素上添加贴图以及对于不规则的元素如何进行操作变换了
-  
+
   const geometry = new THREE.BoxGeometry(1, 1, 1)
-  let texture = new THREE.TextureLoader().load('/texture/file.jpeg')
+  let doorMain = new THREE.TextureLoader().load('/textures/door/color.jpg')
+  let alphaDoor = new THREE.TextureLoader().load('/textures/door/alpha.jpg')
   // texture.wrapS = THREE.RepeatWrapping; // 横向重复
   // texture.wrapT = THREE.RepeatWrapping; // 纵向重复
   // texture.repeat.set( 2, 3 );
   // 但是如果是想将图片贴到单面或是说 一个不规则的位置呢？
-  const matrial = new THREE.MeshBasicMaterial({ map: texture })
-  
+  const matrial = new THREE.MeshBasicMaterial({ 
+    map: doorMain,
+    alphaMap: alphaDoor, // 透明度相乘估计是
+    transparent: true,
+    side: THREE.DoubleSide // 渲染单面还是双面
+  })
+
   const cube = new THREE.Mesh(geometry, matrial)
   scene.add(cube)
-  // 贴图的基本操作
+
 })
 
 </script>
