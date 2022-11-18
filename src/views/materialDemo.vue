c23b2ddc73c5a1514629a3bd904673f658971947
jingshuang1 <jingshuang1@staff.weibo.com>
2022-11-18 16:47:33 +0800

    re

--- src/views/materialDemo.vue
+++ src/views/materialDemo.vue
@@ -4,9 +4,8 @@
 <script setup>
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { onMounted, render } from 'vue'
-import { gsap } from 'gsap'
-import * as dat from 'dat.gui'
+import { onMounted } from 'vue'
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5
